356766b15e78b113f5d1faf57d885f28083baa24
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-06-22 18:36:52 +1000

    Added day.js

---
+++ functions.js
@@ -0,0 +1,34 @@
+function string_message(string) {
+    if (string) {
+      return "The string is nonempty.";
+    } else {
+      return "It's an empty string!";
+    }
+  }
+
+
+  let alt_string_message = (string) => {
+    if (string) {
+      return "The string is nonempty.";
+    } else {
+      return "It's an empty string!";
+    }
+  }
+
+  function name(arg) {
+    //code
+  }
+
+  let name = (arg) => {
+      //code
+  }
+
+
+
+function square(n) {
+    return n ** 2;
+};
+
+let alt_square = (n) => {
+    return n ** 2;
+};
